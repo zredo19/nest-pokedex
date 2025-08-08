ec0fbd0a9fc2ecbf3cfe538fd326e599aa8a987c
Rodolfo Obreque <rodolfoobreque19@gmail.com>
2025-08-08 00:05:04 -0400

    Se modifico el package.json denuevo

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -1,7 +1,7 @@
 import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
-    MONGODB: Joi.required(),
+    MONGODB: Joi.string().required(),
     PORT: Joi.number().default(3005),
     DEFAULT_LIMIT: Joi.number().default(6)
 })
